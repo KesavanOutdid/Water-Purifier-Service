6c522d3d6d93fa867e9f0d60338bc130ef035a4a
sujatavilaskagle <sujata@outdidtech.com>
2025-12-19 12:19:59 +0530

    updated

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -13,7 +13,27 @@ const appRoutes = require('./routes/app/appRoutes');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://192.168.192.1:3000',
+      'http://192.168.0.40:3000',
+    ];
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(null, true);
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
